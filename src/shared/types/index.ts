85aee43acb1dc168c8278721dc90b3ad3c9b17ce
Anton <antonr1@bk.ru>
2024-01-30 17:40:59 +0300

    add mocks data

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,3 +5,4 @@ export { Location } from './location.type.js';
 export { User } from './user.type.js';
 export { Comment } from './comment.type.js';
 export { Offer } from './offer.type.js';
+export { MockServerData } from './mock-server-data.type.js';
